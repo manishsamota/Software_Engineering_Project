b86185580126323c5576571dec5bf254c5c818a7
manish-369 <manishsamota28@gmail.com>
2025-02-05 17:09:17 +0000

    Add all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { CardHeader } from '@mui/material';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+      <div className="App">
+        <h1>
+        This is Header    
+       
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Header;
